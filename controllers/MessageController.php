48eefbcc1b04f1840ea6a33cb11dcee03dd006ad
TahaLechgar <tahamr08@gmail.com>
2022-05-18 11:09:04 +0100

    add Message model and controller

---
+++ controllers/MessageController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+
+class MessageController extends Controller
+{
+
+}
---
+++ models/Message.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+use app\core\Controller;
+
+class Message extends Controller
+{
+
+}
