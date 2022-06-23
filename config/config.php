670354f5d71317429da5c69d6fa9db2d91004147
TahaLechgar <tahamr08@gmail.com>
2022-06-23 10:41:52 +0100

    add cloudinary config

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+define("CLOUDINARY_URL", "cloudinary://573463728199533:pZOSiCrhvFc96ExFohIXmM9qiwY@veinsmoke");
+define("CLOUDINARY_API_KEY", "573463728199533");
+define("CLOUDINARY_API_SECRET", "pZOSiCrhvFc96ExFohIXmM9qiwY");
